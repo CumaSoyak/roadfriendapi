27a8b1f3f8a297e2683807789c2b9d6688a9f2b6
sahin <sahin.hanay@mobillium.com>
2020-03-01 13:50:28 +0300

    auth fix

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -205,7 +205,7 @@ class AuthController extends Controller
      * @param LoginRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function login(LoginRequest $request)
+    public function login(Request $request)
     {
         $credentials = request(['email', 'password']);
         if (!$token = auth('api')->attempt($credentials)) {
