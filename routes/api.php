fac49488d3c8eb71ff418d7c0084bfffb7e690f0
sahin <sahin.hanay@mobillium.com>
2020-03-01 13:44:43 +0300

    auth fix

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Http\Request;
 
 Route::post('auth/register', ['as' => 'auth.register', 'uses' => 'Api\AuthController@register']);
 Route::post('auth/social/{provider}', ['as' => 'auth.social', 'uses' => 'Api\AuthController@social']);
-Route::post('auth/login', ['as' => 'auth.login', 'uses' => 'AuthControllerApi\@login']);
-Route::post('auth/logout', ['as' => 'auth.logout', 'uses' => 'AuthControllerApi\@logout']);
+Route::post('auth/login', ['as' => 'auth.login', 'uses' => 'Api\AuthController@login']);
+Route::post('auth/logout', ['as' => 'auth.logout', 'uses' => 'Api\AuthController@logout']);
